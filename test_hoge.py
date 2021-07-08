11919d5a01fab8942d1710c19881c8da164b5bfe
lacsec <lacsec@yahoo.co.jp>
2021-07-09 00:22:19 +0900

    Update test_hoge.py

--- test_hoge.py
+++ test_hoge.py
@@ -5,10 +5,10 @@ from hoge import my_add, my_sub
 def test_hoge():
   actual = my_add(2,3)
   expected = 5
-  assertEqual(expected, actual)
+  assert expected == actual
   actual = my_sub(2,3)
   expected = 5
-  assertEqual(expected, actual)
+  assert expected == actual
   
 #if __name__ == "__main__":
 #  unittest.main()
