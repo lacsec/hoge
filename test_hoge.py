e1749e308e7a375f420eabd3d892d7a3ed02ca6d
lacsec <lacsec@yahoo.co.jp>
2021-07-09 00:18:24 +0900

    Update test_hoge.py

--- test_hoge.py
+++ test_hoge.py
@@ -3,10 +3,10 @@ from hoge import my_add, my_sub
 
 #class TestMyApp(unittest.TestCase):
 def test_hoge():
-  actual = hoge.my_add(2,3)
+  actual = my_add(2,3)
   expected = 5
   assertEqual(expected, actual)
-  actual = hoge.my_sub(2,3)
+  actual = my_sub(2,3)
   expected = 5
   assertEqual(expected, actual)
   
