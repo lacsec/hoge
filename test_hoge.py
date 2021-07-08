9174027f4a0aaa1eb91b9ddccc9ec02963765bc6
lacsec <lacsec@yahoo.co.jp>
2021-07-09 00:05:24 +0900

    Update test_hoge.py

--- test_hoge.py
+++ test_hoge.py
@@ -6,6 +6,9 @@ class TestMyApp(unittest.TestCase):
     actual = hoge.my_add(2,3)
     expected = 5
     self.assertEqual(expected, actual)
+    actual = hoge.my_sub(2,3)
+    expected = 5
+    self.assertEqual(expected, actual)
   
 if __name__ == "__main__":
   unittest.main()
