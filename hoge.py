11955445a6d585a5ba648326cee94aad0fb93dfb
lacsec <lacsec@yahoo.co.jp>
2021-07-08 23:27:17 +0900

    Create hoge.py

---
+++ hoge.py
@@ -0,0 +1,15 @@
+
+def my_add(a,b):
+  return a+b
+
+def my_sub(a,b):
+  return a-b
+
+def main():
+  print("hello world!")
+  print("1+2",my_add(1,2))
+  print("1-2",my_sub(1,2))
+
+  if __name__ == "__main__":
+  main()
+  
